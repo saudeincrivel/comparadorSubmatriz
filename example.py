46f625295643fac71138e1032ebc2f38f04b8e2c
ENXAQEUCAKRONYCA <willspc@gmail.com>
2022-02-21 07:11:55 -0300

    otimizacao e resize

--- example.py
+++ example.py
@@ -1,14 +1,17 @@
 import os
 import wrapper.wrapper as wrapper
 import imageMatcher
+import time 
 
 
 bigimage = "BIG_IMAGE_PATH";
 smallimage = "SMALL_IMAGE_PATH";
 
 def main():
+    # start_time = time.time();
     # EXAMPLE:
     # x = imageMatcher.matchImages(bigimage, smallimage);
+    # print("--- %s seconds ---" % (time.time() - start_time))
     # print("Occurrences :");
     # print ( x);
     return None;
